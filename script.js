35935f24f2fa65b20f382e9ed20aada897cb4803
K1raQueen <fergusaskeland@gmail.com>
2022-12-06 16:56:11 +0100

    fikset correct

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
 
 
 
-// 60 sec Timer
-
-function countdown() {
-    var seconds = 60;
-    function tick() {
-        var counter = document.getElementById("counter");
-        seconds--;
-        counter.innerHTML =
-            "0:" + (seconds < 10 ? "0" : "") + String(seconds);
-        if (seconds > 0) {
-            setTimeout(tick, 1000);
-        } else {
-
-            document.getElementById("counter").innerHTML = "";
-        }
-    }
-    tick();
-}
-
-// starter timer 
-countdown();
-
 
 
 // score, går opp for hvert riktig svar
@@ -39,8 +17,8 @@ var questions = [
         answer: "script"
     },
     {
-        prompt: "Hvem er gay?",
-        answer: "eddi"
+        prompt: "Hvem er sus?",
+        answer: "red"
     }
 ];
 
@@ -48,47 +26,56 @@ var questions = [
 
 
 
-let myQuestion;
-
-
-// funkksjon som velger et spørsmål 
-function pickQuestion() {
-
-
 
     // variabel som gjør at spørsmålene som blir valgt er tilvelig hver gang
-    myQuestion = questions[Math.floor(Math.random() * questions.length)]
+    
+    let myQuestion = questions[Math.floor(Math.random() * questions.length)]
+    
 
 
-    return (myQuestion);
+    // funkksjon som velger et spørsmål 
+    function pickQuestion() {
 
+    
+        return (myQuestion);
+    
 
+    
 }
 
 
+    
+    
+    // henter overskriften på siden og lagrer det i en variabel
+    var questionHeader = document.getElementById("questionHeader");
+    
+    // bytter skriften i overskriften til spørsmålet valgt i pickquesition
+    questionHeader.innerHTML = pickQuestion().prompt
+    
 
 
-// henter overskriften på siden og lagrer det i en variabel
-var questionHeader = document.getElementById("questionHeader");
 
-// bytter skriften i overskriften til spørsmålet valgt i pickquesition
-questionHeader.innerHTML = pickQuestion().prompt
+
+
 
 
 // funksjon som gjør at hvis det man skriver inn på siden er det samme som svaret til spørsmålet får man pluss ett poeng
 function myFunction() {
-    var response = document.getElementById("response").value
-    var answer = pickQuestion().answer
 
-    if (response === answer) {
-        score++;
-        alert('correct');
-    } else {
-        alert('wrong');
-    }
+        var response = document.getElementById("response").value
+        var answer = pickQuestion().answer
 
+        if (response === answer) {
+            score++;
+            alert('correct');
+        } else {
+            alert('wrong');
+        }
+    
 }
 
+
+alert("you got " + score + "/" + questions.length + " questions right");
 
 
 
