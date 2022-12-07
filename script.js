a9e48a7c5ad4afffa895c68ccba42a76a818278a
K1raQueen <fergusaskeland@gmail.com>
2022-12-07 21:35:24 +0100

    Fikset spillet og addet gifs

--- script.js
+++ script.js
@@ -2,28 +2,106 @@
 
 
 
-
-// score, går opp for hvert riktig svar
+// Scoren til spilleren, vil gå opp for hvert riktig svar
 var score = 0;
 
-// spørsmålene 
+
+// Spørsmålene 
 var questions = [
     {
-        prompt: "Hva ar en par",
-        answer: "aper"
+        prompt: "Hva koster 50 kroner og har sjokolade på seg?",
+        answer: "par"
     },
     {
-        prompt: "Fullfør setningen: java",
-        answer: "script"
+        prompt: "Hva er den beste monster",
+        answer: "pacific"
     },
     {
-        prompt: "Hvem er sus?",
-        answer: "red"
+        prompt: "Hvilken type male er Iulian",
+        answer: "beta"
     }
+
 ];
 
 
+// Variabel looper gjennom de forskjellige spørsmålene
+var i = 0;
 
+
+// Starter funksjonen når siden åpnes
+pickQuestionHeader();
+
+
+// Funskjonen gjør at overskriften blir byttet til det valgte spørsmålet
+function pickQuestionHeader() {
+
+    var questionHeader = document.getElementById("questionHeader");
+
+    questionHeader.innerHTML = questions[i].prompt
+}
+
+
+
+// Funksjon som sammenligner user input til svaret på spørsmålet og går til neste sprsmål med mindre quiz er ferdig
+function answerCompare() {
+
+    var response = document.getElementById("response").value
+    var answer = questions[i].answer
+
+    if (response === answer) {
+        score++;
+    }
+
+    i++;
+
+    if (i < questions.length) {
+        pickQuestionHeader()
+    } else {
+        afterSubmit();
+    }
+
+}
+
+
+// Funksjon for svaret etter man er ferdig med quizen
+
+function afterSubmit() {
+
+    var pictures = ["images/walter-white-falling.gif", "images/american_pyscho.gif", "images/homelander.gif", "images/gigachad.gif"];
+
+    document.getElementById("after_submit").style.visibility = "visible";
+
+    document.getElementById("number_correct").innerHTML = "You got " + score + "/" + questions.length + " correct.";
+
+    document.getElementById("picture").src = pictures[score];
+
+}
+
+
+
+/*
+for (var i=0; i < questions.length; i++ ) {
+
+
+  
+
+    document.write("<h1>"+ questions[i].prompt + "</h1>")
+
+    
+    
+    var answer = questions[i].answer
+
+   
+    var response = document.getElementById("response").value
+
+    if(response == answer) {
+      score++;
+      alert("wallah")
+      
+    } else {
+     
+    } 
+  }
 
 
 
@@ -39,12 +117,9 @@ var questions = [
     
         return (myQuestion);
     
-
-    
 }
 
 
-    
     
     // henter overskriften på siden og lagrer det i en variabel
     var questionHeader = document.getElementById("questionHeader");
@@ -57,10 +132,8 @@ var questions = [
 
 
 
-
-
 // funksjon som gjør at hvis det man skriver inn på siden er det samme som svaret til spørsmålet får man pluss ett poeng
-function myFunction() {
+function answerCompare() {
 
         var response = document.getElementById("response").value
         var answer = pickQuestion().answer
@@ -75,13 +148,17 @@ function myFunction() {
 }
 
 
+
+
+
+function wallah(){
 alert("you got " + score + "/" + questions.length + " questions right");
+}
 
 
 
 
-
-/* for (var i=0; i < questions.length; i++ ) {
+ for (var i=0; i < questions.length; i++ ) {
    var response = window.prompt(questions[i].prompt);
    if(response == questions[i].answer) {
      score++;
